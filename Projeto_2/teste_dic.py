9ffbd13662304f5e40506bc5d0ee2bafb8d9640a
Douglas Fonseca <douglas.barbosa.fonseca@gmail.com>
2022-06-15 12:28:13 -0300

    cadastro de musicos

--- Projeto_2/teste_dic.py
+++ Projeto_2/teste_dic.py
@@ -20,6 +20,8 @@ def castrar_1_musico() -> dict:
         lista_instrumentos.append(resposta)
     musico['instrumento'] = lista_instrumentos
 
+    return musico
+
 def cadastrar_musicos():
     try:
         with open("Projeto_2/cadastro_musicos.json", "r", encoding="utf8") as arquivo:
@@ -32,30 +34,4 @@ def cadastrar_musicos():
     with open("Projeto_2/cadastro_musicos.json", "w", encoding="utf8") as arquivo:
          arquivo.write(json.dumps(cadastro_geral, indent=4))
 
-def buscar_musicos():
-    ...
-def modificar_musicos():
-    ...
-def montar_bandas():
-    ...
-
-def menu():
-    opcoes = {
-        "1": cadastrar_musicos,
-        "2": buscar_musicos,
-        "3": modificar_musicos,
-        "4": montar_bandas,
-        "0": "Sair"
-    }
-    
-    opcao_usuario = "xxx"
-    while opcao_usuario != "0":
-        print("Bem vindo ao programa de formação de bandas, escolha uma opção:")
-        print("1 - Cadastrar Músicos\n2 - Buscar Músicos\n3 - Modificar Músicos\n4 - Montar Bandas\n0 - Sair")
-        opcao_usuario = input()
-        try:
-            ...
-        except:
-            print("opcao inválida")
-
-menu()
+cadastrar_musicos()
