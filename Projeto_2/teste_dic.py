38e243d5f5529bc66c792a4a68b94ccf59b032cc
Douglas Fonseca <douglas.barbosa.fonseca@gmail.com>
2022-06-17 21:25:05 -0300

    ideia de combinacao de bandas

--- Projeto_2/teste_dic.py
+++ Projeto_2/teste_dic.py
@@ -1,46 +1,24 @@
+from Projeto_2.Projeto_2_main import busca_e
 
-import json
-from textwrap import indent
+def criar_tuplas(aptos_para_banda) -> list[tuple]:
+    lista_tuplas = []
+    for lista in aptos_para_banda:
+        for musico in lista:
+            lista_tuplas.append((musico['email'],musico['instrumento']))
+
+    return lista_tuplas
 
 
-
-def modificador(email_musico, genero_ou_instrumento, add_ou_remover):
+def montar_bandas():
+    genero_banda = input("\nQual o gênero da sua banda?")
     try:
-        with open("Projeto_2/cadastro_musicos.json", "r", encoding="utf8") as arquivo:
-            cadastro_geral = json.load(arquivo)
-        #cadastro_geral = abridor_modo_ler()
-    except:
-        print("Não foi possível abrir o arquivo.")
-
-    if add_ou_remover == "adicionar":
-        for musico in cadastro_geral:
-            if musico['email'] == email_musico:
-                print(f"\nQual {genero_ou_instrumento} deseja {add_ou_remover}?")
-                print(musico[genero_ou_instrumento])
-                musico[genero_ou_instrumento].append(input())
-                return musico
-    else:
-        for musico in cadastro_geral:
-            if musico['email'] == email_musico:
-                print(f"\nQual {genero_ou_instrumento} deseja {add_ou_remover}?")
-                print(musico[genero_ou_instrumento])
-                try:
-                    musico[genero_ou_instrumento].pop(musico[genero_ou_instrumento].index(input()))
-                except:
-                    print(f"Não é possível remover este {genero_ou_instrumento} pois ele não está cadastrado!")
-
-def modificar_musicos():      
-    email_musico = input("\nDigite o e-mail do músico que deseja modificar:")
+        tamanho_banda = int(input("\nQuantos músicos tocarão na banda?"))
+    except ValueError:
+        return print("Erro, o número de músicos deve ser um inteiro.")
     
-    genero_ou_instrumento = ""
-    while genero_ou_instrumento != "genero" and genero_ou_instrumento != "instrumento":
-        genero_ou_instrumento = input("\nDeseja modificar um gênero ou um instrumento?(GENERO/INSTRUMENTO)").lower()
-    add_ou_remover = ""
-    while add_ou_remover != "adicionar" and add_ou_remover != "remover":
-        add_ou_remover = input(f"\nDeseja adicionar ou remover o {genero_ou_instrumento}?(ADICIONAR/REMOVER)").lower()
-
-    modificador(email_musico, genero_ou_instrumento, add_ou_remover)
-    
+    instrumentos_banda = [input(f"\nDigite o instrumento de número {i+1}: ") for i in range(tamanho_banda)]
+    aptos_para_banda = [busca_e({'genero':genero_banda, 'instrumento':instr}) for instr in instrumentos_banda]
+    lista_tuplas = criar_tuplas(aptos_para_banda)
 
 
 
--- Projeto_2/teste_digit.py
+++ Projeto_2/teste_digit.py
@@ -14,18 +14,32 @@
 #         print("E-mail deve conter apenas letras, '_', '.', números e exatamente 1 '@'")
 #         raise Exception
 # if not (soma == 1):
-#     raise Exception
+# #     raise Exception
 
-dic1 = {"a":"aa"}
-dic2 = {"b":"ab"}
-dic3 = {"c":[1,2,3,4,5,6]}
+# dic1 = {"a":"aa"}
+# dic2 = {"b":"ab"}
+# dic3 = {"c":[1,2,3,4,5,6]}
 
-dict_vazio = {}
-dict_vazio["teste"] = "teste1"
-dict_vazio["teste2"] = "teste12"
-dict_vazio["teste3"] = "teste123"
-print(len(dict_vazio))
-# for i in dict_vazio:
-#     print(i)
-dic3["c"].pop(1)
-print(dic3)
+# dict_vazio = {}
+# dict_vazio["teste"] = "teste1"
+# dict_vazio["teste2"] = "teste12"
+# dict_vazio["teste3"] = "teste123"
+# print(len(dict_vazio))
+# # for i in dict_vazio:
+# #     print(i)
+# dic3["c"].pop(1)
+# print(dic3)
+
+# tamanho_banda = 3
+
+# instrumentos_banda = []
+# for i in range(tamanho_banda):
+#     instrumentos_banda.append(input(f"\nDigite o instrumento de número {i+1}"))
+
+# print(instrumentos_banda)
+
+# instrumentos_banda = [input(f"\nDigite o instrumento de número {i+1}") for i in range(tamanho_banda)]
+# print(instrumentos_banda)
+lista = []
+lista.append((1,2))
+print(lista)
